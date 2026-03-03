d9feeb13c80d5c59bab5f0c0869fc4115d67cbe3
Tim <tdp378@gmail.com>
2026-03-03 13:47:39 -0500

    gimble mode

--- src/jax_controller/scripts/jax_teleop_body.py
+++ src/jax_controller/scripts/jax_teleop_body.py
@@ -6,14 +6,16 @@ import sys, select, termios, tty
 import math
 
 msg = """
-JAX IK - Full Control (Fixed Wave & Sit)
+JAX IK - GIMBAL MASTER (Symmetry Fix)
 ---------------------------
-W / S : Lean Forward / Back
-A / D : Lean Left / Right
+W / S : Pitch Forward / Back
+A / D : Roll Left / Right
+I / K : SLIDE Body Forward / Back
+J / L : SLIDE Body Left / Right
 R / F : Change Height
-H     : TOGGLE WAVE
-K     : TOGGLE SIT
-T     : TEST STEP (Front Left)
+N     : TOGGLE SIT
+M     : TOGGLE WAVE
+T     : TEST STEP
 Space : Reset All
 CTRL-C to quit
 """
@@ -23,21 +25,18 @@ class JaxTeleopBody(Node):
         super().__init__('jax_teleop_body')
         self.pub = self.create_publisher(JointTrajectory, '/joint_trajectory_controller/joint_trajectory', 10)
         
-        # --- Physical Constants ---
-        self.L1 = 0.130011  
-        self.L2 = 0.132389  
-        self.perfect_h = 0.18  
+        self.L1, self.L2 = 0.130011, 0.132389
+        self.perfect_h = 0.18
         
-        # State Variables
         self.pitch, self.roll, self.height = 0.0, 0.0, 0.18
         self.t_pitch, self.t_roll, self.t_height = 0.0, 0.0, 0.18
         
-        self.is_waving = False
-        self.wave_step = 0.0
-        self.is_sitting = False
-        self.sit_lerp = 0.0
-        self.is_test_stepping = False
-        self.step_phase = 0.0 
+        self.body_x, self.body_y = 0.0, 0.0
+        self.t_body_x, self.t_body_y = 0.0, 0.0
+        
+        self.is_waving, self.wave_step = False, 0.0
+        self.is_sitting, self.sit_lerp = False, 0.0
+        self.is_test_stepping, self.step_phase = False, 0.0 
         
         self.joint_names = [
             'fl_hip_joint', 'fl_thigh_joint', 'fl_calf_joint',
@@ -57,12 +56,9 @@ class JaxTeleopBody(Node):
         
         cos_c_targ = (l1**2 + l2**2 - d_target**2) / (2 * l1 * l2)
         abs_calf_targ = -(math.pi - math.acos(max(-1.0, min(1.0, cos_c_targ))))
-        
         cos_t_targ = (l1**2 + d_target**2 - l2**2) / (2 * l1 * d_target)
-        thigh_int = math.acos(max(-1.0, min(1.0, cos_t_targ)))
-        abs_thigh_targ = (thigh_int - math.atan2(x, z))
+        abs_thigh_targ = (math.acos(max(-1.0, min(1.0, cos_t_targ))) - math.atan2(x, z))
         
-        # Reference (0.18m)
         d_perf = self.perfect_h
         cos_c_perf = (l1**2 + l2**2 - d_perf**2) / (2 * l1 * l2)
         abs_calf_perf = -(math.pi - math.acos(max(-1.0, min(1.0, cos_c_perf))))
@@ -82,56 +78,50 @@ class JaxTeleopBody(Node):
         traj = JointTrajectory()
         traj.joint_names = self.joint_names
         
-        # 1. SMOOTHING
+        # Smooth Transitions
         self.pitch += (self.t_pitch - self.pitch) * 0.15
         self.roll += (self.t_roll - self.roll) * 0.15
         self.height += (self.t_height - self.height) * 0.15
+        self.body_x += (self.t_body_x - self.body_x) * 0.15
+        self.body_y += (self.t_body_y - self.body_y) * 0.15
 
-        # Smooth Sit Transition
         if self.is_sitting and self.sit_lerp < 1.0: self.sit_lerp += 0.05
         elif not self.is_sitting and self.sit_lerp > 0.0: self.sit_lerp -= 0.05
 
-        # 2. COORDINATE PREP
-        # Normal Body Height Logic
+        # 1. Coordinate Prep
         z_fl = self.height - (self.pitch * 0.1) + (self.roll * 0.1)
         z_fr = self.height - (self.pitch * 0.1) - (self.roll * 0.1)
         z_rl = self.height + (self.pitch * 0.1) + (self.roll * 0.1)
         z_rr = self.height + (self.pitch * 0.1) - (self.roll * 0.1)
         
-        # Sitting Logic (Front Legs stay at 0.23m, Rear Legs lerp down to sit height)
+        # 2. Sit Logic
         z_fl = z_fl * (1 - self.sit_lerp) + 0.23 * self.sit_lerp
         z_fr = z_fr * (1 - self.sit_lerp) + 0.23 * self.sit_lerp
-        # Apply SIT LERP (Front stays up, Rear goes down)
-        sit_height = 0.10 
-        z_rl = z_rl * (1 - self.sit_lerp) + sit_height * self.sit_lerp
-        z_rr = z_rr * (1 - self.sit_lerp) + sit_height * self.sit_lerp
+        z_rl = z_rl * (1 - self.sit_lerp) + 0.10 * self.sit_lerp
+        z_rr = z_rr * (1 - self.sit_lerp) + 0.10 * self.sit_lerp
 
-        # 3. TEST STEP OVERRIDE (Front Left)
-        step_x, step_z_off = 0.0, 0.0
+        # 3. Hip/Gimbal Y-Logic (SYMMETRY FIX)
+        hip_angle = math.atan2(self.body_y, self.height)
+        fl_h = fr_h = hip_angle
+        rl_h = rr_h = -hip_angle # Mirroring the rear to match front motion
+
+        # 4. IK solving for X/Z
+        step_x = 0.0
         if self.is_test_stepping:
             self.step_phase += 0.1
-            if self.step_phase > 1.0: 
-                self.step_phase = 0.0
-                self.is_test_stepping = False
-            step_z_off = -0.04 * math.sin(math.pi * self.step_phase)
+            if self.step_phase > 1.0: self.is_test_stepping = False
             step_x = -0.05 * math.sin(math.pi * self.step_phase)
-            z_fl += step_z_off
+            z_fl -= 0.04 * math.sin(math.pi * self.step_phase)
 
-        # 4. SOLVE IK
-        fl_t, fl_c = self.solve_ik(step_x, z_fl)
-        fr_t, fr_c = self.solve_ik(0, z_fr)
-        rl_t, rl_c = self.solve_ik(0, z_rl)
-        rr_t, rr_c = self.solve_ik(0, z_rr)
-        
-        fl_h = fr_h = rl_h = rr_h = 0.0
+        fl_t, fl_c = self.solve_ik(step_x - self.body_x, z_fl)
+        fr_t, fr_c = self.solve_ik(0 - self.body_x, z_fr)
+        rl_t, rl_c = self.solve_ik(0 - self.body_x, z_rl)
+        rr_t, rr_c = self.solve_ik(0 - self.body_x, z_rr)
 
-        # 5. WAVE OVERRIDE (Total Manual Control for Wave)
+        # 5. Wave Override
         if self.is_waving:
             self.wave_step += 0.3
-            # Lift FL high and tuck calf
-            fl_t = -1.0
-            fl_c = -1.8
-            fl_h = 0.4 * math.sin(self.wave_step)
+            fl_t, fl_c, fl_h = -1.0, -1.8, 0.4 * math.sin(self.wave_step)
 
         point = JointTrajectoryPoint()
         point.positions = [fl_h, fl_t, fl_c, fr_h, fr_t, fr_c, rl_h, rl_t, rl_c, rr_h, rr_t, rr_c]
@@ -150,19 +140,19 @@ def main():
             elif key == 's': node.t_pitch -= 0.05
             elif key == 'a': node.t_roll -= 0.05
             elif key == 'd': node.t_roll += 0.05
+            elif key == 'i': node.t_body_x += 0.01
+            elif key == 'k': node.t_body_x -= 0.01
+            elif key == 'j': node.t_body_y -= 0.01
+            elif key == 'l': node.t_body_y += 0.01
             elif key == 'r': node.t_height = min(0.24, node.t_height + 0.01)
             elif key == 'f': node.t_height = max(0.08, node.t_height - 0.01)
-            elif key == 'h': 
-                node.is_waving = not node.is_waving
-                if not node.is_waving: node.wave_step = 0.0
-            elif key == 'k': node.is_sitting = not node.is_sitting
-            elif key == 't': 
-                node.is_test_stepping = True
-                node.step_phase = 0.0
+            elif key == 'm': node.is_waving = not node.is_waving
+            elif key == 'n': node.is_sitting = not node.is_sitting
+            elif key == 't': node.is_test_stepping, node.step_phase = True, 0.0
             elif key == ' ': 
-                node.t_pitch, node.t_roll, node.t_height = 0.0, 0.0, 0.18
-                node.is_sitting = False
-                node.is_waving = False
+                node.t_pitch = node.t_roll = node.t_body_x = node.t_body_y = 0.0
+                node.t_height = 0.18
+                node.is_sitting = node.is_waving = False
             elif key == '\x03': break
             rclpy.spin_once(node, timeout_sec=0.01)
     finally:
